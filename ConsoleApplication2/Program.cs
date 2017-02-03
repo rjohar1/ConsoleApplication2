fac1aa66e57b06aec5378e68bd299945e4383023
Samar <rjohar@hotmail.com>
2017-02-03 09:35:40 -0800

    localize in en_US

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi y'All");
         }
     }
 }
